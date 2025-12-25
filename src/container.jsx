bd104261621d76a7b7d7d265c9f198a0cb40cdc2
manu-hn <manuu.hn11@gmail.com>
2025-12-25 19:42:54 +0530

    `Update dependencies and configuration for React, Vite, and Tailwind CSS`

---
+++ src/container.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "./components/ui/navbar"
+import Footer from "./components/ui/footer"
+
+const BodyContainer = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="font-semibold text-red-600">Merge - Where Developers Connect</h1>
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default BodyContainer

5cf22ec88a43243e1cd8a054dbfa2bd814e6f981
manu-hn <manuu.hn11@gmail.com>
2026-03-01 14:18:02 +0530

    `Updated constants.js: changed BASE_URL from VITE_APP_BASE_URL to "/api" and removed export keyword`

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+// export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+export const BASE_URL = "/api";
+
 export const USER_ICON_IMAGE=`https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp`;
 
 

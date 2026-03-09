f6e67f6b88a17ee1f802c9c7e326f0ad99447952
manu-hn <manuu.hn11@gmail.com>
2026-03-09 21:50:12 +0530

    `Update BASE_URL constant with conditional logic`

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
-// export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
-export const BASE_URL = "/api/";
+// export const BASE_URL = "/api/";
+
+
+export const BASE_URL = location.hostname==="localhost" ? import.meta.env.VITE_APP_BASE_URL :"/api/" ;
+
 
 export const USER_ICON_IMAGE=`https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp`;
 
